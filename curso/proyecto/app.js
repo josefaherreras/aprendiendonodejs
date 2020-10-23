124ee98f6dd31bbeb4c4ad282c7eca0c9a8bc238
Josefa Herrera Sepúlveda <Josefa@MacBook-Pro.local>
2020-10-23 02:08:06 -0300

    instalación de mongodb y la limongoose

--- curso/proyecto/app.js
+++ curso/proyecto/app.js
@@ -1,9 +1,28 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");//mapeando una clase que es un modelo con una tabla.
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/fotos",
+ {useNewUrlParser: true}) .then(() => { console.log("La Conexion a MongoDB se ha realizado Conrrectamente!!"); }) .catch(err => console.log(err));
+
+var userSchemaJSON = {
+    email: String,
+    password: String
+}
+
+var user_schema = new Schema(userSchemaJSON);//crear objeto / estructura de nuestra tabla
+
+//modelo con la conexion a la base de datos
+var User = mongoose.model("User",user_schema);
 
 //middlewares
-app.use(express.static('public'));
-app.use(express.static('assets'));//archivos que no cambian , son estaticos
+app.use(express.static('public'));//archivos que no cambian , son estaticos
+
+app.use(bodyParser.json());//extraer parametros / peticiones application/json
+app.use(bodyParser.urlencoded({extended: true}));// parsing de la libreria 
+
 
 //motor de vista
 app.set("view engine","jade");
@@ -14,7 +33,22 @@ app.get("/", function (req,res) {
 });
 
 app.get("/login", function (req,res) {
-    res.render("login");
+    User.find(function(err,doc){
+        console.log(doc);
+        res.render("login");
+    })
+});
+
+app.post("/users", function(req,res){
+    // console.log("Email :" + req.body.email);
+    // console.log("Contraseña :" + req.body.password);
+
+    var user = new User({email: req.body.email, password: req.body.password});
+
+    //guardar usuario
+    user.save(function(){
+        res.send("Guardamos tus datos");
+    });
 });
 
 
