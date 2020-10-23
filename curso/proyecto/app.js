77728061ea731a7c968a46530d3ce6ae2d32b677
Josefa Herrera Sepúlveda <Josefa@MacBook-Pro.local>
2020-10-23 16:13:59 -0300

    validación de contraseña con virtuals

--- curso/proyecto/app.js
+++ curso/proyecto/app.js
@@ -1,33 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var mongoose = require("mongoose");//mapeando una clase que es un modelo con una tabla.
-// ¿Qué es la Mongoose? Mangosta es un Object Document Mapper (ODM).
-// Esto significa que Mongoose le permite definir objetos con un esquema fuertemente tipado
-// que se asigna a un documento MongoDB. Mongoose proporciona una increíble cantidad de funcionalidades
-// para crear y trabajar con esquemas.
-var Schema = mongoose.Schema;
-
-mongoose.connect("mongodb://localhost:27017/fotos",
- {useNewUrlParser: true}) .then(() => { console.log("La Conexion a MongoDB se ha realizado Conrrectamente!!"); }) .catch(err => console.log(err));
+//retornando : module.exports.User= User;
+var User = require("./models/user").User; //nos devuelve todo el objeto
 
 
- //los esquemas corresponden a una coleccion a la base de datos en mongodb, la funcionalidad 
- //de los esquemas es definir la forma que tienen nuestros documentos en la coleccion de mongo
-
- //esquema => colecciones => tablas
- //documentos => filas
-var userSchemaJSON = {
-    email: String,
-    password: String
-}
-
-var user_schema = new Schema(userSchemaJSON);//crear objeto / estructura de nuestra tabla
-
-//modelo con la conexion a la base de datos
-var User = mongoose.model("User",user_schema);
-
-//middlewares
+ //middlewares
 app.use(express.static('public'));//archivos que no cambian , son estaticos
 
 app.use(bodyParser.json());//extraer parametros / peticiones application/json
@@ -53,7 +31,11 @@ app.post("/users", function(req,res){
     // console.log("Email :" + req.body.email);
     // console.log("Contraseña :" + req.body.password);
 
-    var user = new User({email: req.body.email, password: req.body.password});
+    var user = new User({email: req.body.email,
+                            password: req.body.password,
+                            password_confirmation : req.body.password_confirmation
+                        });
+    console.log(user.password_confirmation);
 
     //guardar usuario
     user.save(function(){
--- curso/proyecto/models/user.js
+++ curso/proyecto/models/user.js
@@ -1,11 +1,23 @@
 var mongoose = require("mongoose");
 //mongoose tiene un atributo que mapea que nos retorna un objeto que es el atributo schema
 //este es un constructor para que podamos generar nuestros esquemas
+//mapeando una clase que es un modelo con una tabla.
+// ¿Qué es la Mongoose? Mangosta es un Object Document Mapper (ODM).
+// Esto significa que Mongoose le permite definir objetos con un esquema fuertemente tipado
+// que se asigna a un documento MongoDB. Mongoose proporciona una increíble cantidad de funcionalidades
+// para crear y trabajar con esquemas.
 var Schema = mongoose.Schema;
+
+//conexion a la base de datos
+mongoose.connect("mongodb://localhost:27017/fotos",
+ {useNewUrlParser: true}) .then(() =>
+  { console.log("La Conexion a MongoDB se ha realizado Conrrectamente!!"); }) .catch(err => console.log(err));
+
 
 //esquema
 var user_schema = new Schema({
     name: String,
+    last_name: String,
     username: String,
     password: String,
     age: Number,
@@ -14,7 +26,7 @@ var user_schema = new Schema({
 });//crear objeto / estructura de nuestra tabla
 
 
-//definición de un documento:
+//definición de un documento: 
 //itpos de datos que se pueden guardar en la base de mongodb atravez de mongoose
 /*
 String
@@ -26,3 +38,57 @@ Mixed
 Objetc id
 Array
 */
+
+//Documentos: virtuals propiedades de un documentos
+//validaciones 
+//getters and setters -> acceder a un atributo
+//get -> la forma en la que se accede a un atributo
+//set -> establecer la logica a la cual se asigna un valor a un atributo
+user_schema.virtual("password_confirmation").get(function(){
+    return this.p_c;
+}).set(function(password){
+    this.p_c = password;
+});
+
+user_schema.virtual("full_name").get(function(){
+    return this.name + this.last_name;
+}).set(function(full_name){
+    var words = full_name.split(" ");
+    this.name = words[0];
+    this.last_name = words[1];
+})
+
+/*MODELOS*/
+//SON INSTANCIAS EN MONGOOSE QUE NOS PERMITEN LLAMAR METODOS QUE A LA VEZ NOS PERMITEN 
+//EJECTUCAR ACCIONES SOBRE LA BD SIN LA NECESIDAD QUE NOSOTROS ENTENDAMOS LO QUE ESTÁ PASANDO EN LA BD COMO TAL.
+
+
+//esto equivale a una tabla
+//User define el nombre de la coleccion pero lo toma en plural
+var User = mongoose.model("User",user_schema);
+
+//para conectarse a la basde de datos de Mongo
+//es atravez de modelos 
+module.exports.User= User;
+
+
+
+
+
+
+//ejemplo
+/*
+ //los esquemas corresponden a una coleccion a la base de datos en mongodb, la funcionalidad 
+ //de los esquemas es definir la forma que tienen nuestros documentos en la coleccion de mongo
+
+ //esquema => colecciones => tablas
+ //documentos => filas
+ var userSchemaJSON = {
+    email: String,
+    password: String
+}
+
+var user_schema = new Schema(userSchemaJSON);//crear objeto / estructura de nuestra tabla
+
+//modelo con la conexion a la base de datos
+var User = mongoose.model("User",user_schema);*/
