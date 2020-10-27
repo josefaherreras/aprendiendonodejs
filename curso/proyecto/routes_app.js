1b887412e54902a4f873f418b2d5d8f417545e69
Josefa Herrera Sepúlveda <Josefa@MacBook-Pro.local>
2020-10-27 12:27:58 -0300

    Rutas para CRUD

--- curso/proyecto/routes_app.js
+++ curso/proyecto/routes_app.js
@@ -11,6 +11,44 @@ router.get("/",function(req,res){
 
 
 
+//rutas
+
+//despliega el formulario para crear
+router.get("/imagenes/new", function(req,res){
+    res.render("app/imagenes/new");
+});
+
+//despliega el formulario de una imagen ya existente que nos va a permitir dicha imagen
+router.get("/imagenes/:id/edit", function(req,res){
+
+});
+
+/*REST*/
+
+router.route("/imagenes/:id")
+    .get(function(req,res){
+
+    })
+    .put(function(req,res){
+
+    })
+    .delete(function(req,res){
+        
+    });
+
+
+//coleccion de imagenes 
+//obtener todas las imagenes
+router.route("/imagenes/")
+    .get(function(req,res){
+
+    })
+//crear una nueva imagen
+    .post(function(req,res){
+
+    });
+
+
 
 
 
